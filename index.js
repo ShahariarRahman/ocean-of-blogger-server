4f70e5e67e761c9e734a1b849a38c79d612f2240
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-02 09:28:27 +0600

    mongodb connection build successful

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-const app = express()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+
+const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@blogs.rzvskta.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    client.close();
+    console.log("connection okk!")
+});
 
 
 app.get('/', (req, res) => {
@@ -10,4 +28,4 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-})
+});
