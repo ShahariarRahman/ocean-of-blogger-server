ecd2703d827567ced29df12d0b32a1f40efe69c9
ShahariarRahman <shahariarrahman98@gmail.com>
2023-01-02 09:52:09 +0600

    C:/Program Files/Git/blogs api ready to get all blogs

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,15 +10,21 @@ app.use(cors());
 app.use(express.json());
 
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@blogs.rzvskta.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    client.close();
-    console.log("connection okk!")
-});
+
+const run = async () => {
+    try {
+        await client.connect();
+        const blogsCollection = client.db("blogs-database").collection("blogs-collection");
+        app.get("/blogs", async (req, res) => {
+            const blogs = await blogsCollection.find().toArray();
+            res.send(blogs);
+        });
+    }
+    finally { }
+};
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
